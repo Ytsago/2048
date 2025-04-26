cace2fabf7c3221a4d0eaf3f1c658038dd796180
Sebastien Cros <secros@z4r10p3.42lyon.fr>
2025-04-26 09:43:39 +0200

    Default name and grid

--- inc/wong_car_wai.h
+++ inc/wong_car_wai.h
@@ -6,11 +6,28 @@
 /*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 09:13:16 by secros            #+#    #+#             */
-/*   Updated: 2025/04/26 09:15:06 by secros           ###   ########.fr       */
+/*   Updated: 2025/04/26 09:42:47 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WONG_CAR_WAI_H
 # define WONG_CAR_WAI_H
+
+typedef struct	s_player	t_player;
+
+# define NAME_D "Unknow"
+
+enum e_const
+{
+	WIN_VALUE = 2048
+};
+
+struct t_player
+{
+	char	*name;
+	size_t	score;
+	int		**grid;
+	int		size;
+};
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,29 @@
 #include <ncurses.h>
+#include "libft.h"
 #include "wong_car_wai.h"
 
 int main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	int		**grid;
+	int		i = 0;
+	int		size;
+	char	*name;
+
+	if (ac == 1 || ac > 2)
+		return (1);
+	size = ft_atoi(av[1]);
+	grid = ft_calloc(sizeof(int *), size);
+	if (!grid)
+		return (1);
+	while (i < size)
+	{
+		grid[i] = ft_calloc(sizeof(int), size);
+		if (!grid[i])
+		i++;
+	}
+	i = 0;
+	while (i < size)
+		free(grid[i++]);
+	free(grid);
 	return (0);
 }

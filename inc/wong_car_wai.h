b0cc38df5e5d7f82e4cf10f79f24ab29de7b6483
Sebastien Cros <secros@z4r10p3.42lyon.fr>
2025-04-26 15:46:39 +0200

    attempt to highscore

--- inc/wong_car_wai.h
+++ inc/wong_car_wai.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wong_car_wai.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lilefebv <lilefebv@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 09:13:16 by secros            #+#    #+#             */
-/*   Updated: 2025/04/26 15:43:04 by lilefebv         ###   ########lyon.fr   */
+/*   Updated: 2025/04/26 15:46:20 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@
 extern int sig_global;
 
 # define NAME_D "Unknow"
+# define FILENAME "highscore.txt"
+# define BOARD_SIZE 5
 
 typedef struct	s_player	t_player;
 
--- src_bonus/highscore.c
+++ src_bonus/highscore.c
@@ -6,25 +6,79 @@
 /*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 14:43:30 by secros            #+#    #+#             */
-/*   Updated: 2025/04/26 15:06:04 by secros           ###   ########.fr       */
+/*   Updated: 2025/04/26 15:41:44 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include "get_next_line.h"
 #include "wong_car_wai.h"
+#include "ft_printf.h"
 
-int	get_highscore(char *filename, t_player *player)
+t_player	convert_to_board(char *str)
 {
-	int	fd = open(filename, O_RDWR | O_CREAT, 0644);
+	t_player	new;
+	int			i = 0;
+	
+	while(str[i])
+	{
+		if (str[i] == ':')
+			str[i] = '\0';
+		new.name = str;
+		new.score = ft_atol(&str[i + 1]);
+	}
+	return (new);
+}
 
+// int	lowest_score(t_high *board)
+// {
+// 	int	i = 0;
+// 	size_t	max;
+	
+// 	while (i < BOARD_SIZE)
+// 	{
+		
+// 	}
+// }
+
+int	get_highscore(char *filename, t_high *board)
+{
+	int		fd;
+	char	*str;
+	char	*tmp;
+	int		i = 0;
+
+	fd = open(filename, O_RDWR | O_CREAT, 0644);
 	if (fd == -1)
 		return (1);
-	
-	
+	while ((str = get_next_line(fd)))
+	{
+		if (i < BOARD_SIZE)
+			board->best[i] = convert_to_board(str);
+		else
+			free(str);
+		i++;
+	}
+}
+
+void	display_board(t_high board)
+{
+	int	i = 0;
+	while (i < BOARD_SIZE)
+	{
+		if (!board.best[i].name)
+			break ;
+		ft_printf("%s :", board.best[i].name);
+		ft_printf(" %s\n", ft_itoa(board.best[i].score));
+		i++;
+	}
 }
 
 int	main()
 {
-	
+	t_high	board;
+
+	ft_bzero(&board, sizeof(board));
+	get_highscore(FILENAME, &board);
+	display_board(board);
 }
